708917459afffa7d517d89ec1da9675ea503a7ed
[Atur-js] <[Atour127@gmail.com]>
2021-05-10 20:19:10 +0430

    First commit

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img 
+                className="nav__logo"
+                src="https://www.dlf.pt/dfpng/middlepng/36-368532_netflix-hd-png-download.png"
+                alt="Netflix Logo"
+            />
+            <img
+                className="nav__avatar"
+                src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            />
+        </div>
+    )
+}
+
+export default Nav;
